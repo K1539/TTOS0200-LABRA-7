367b1d0510d317e5c36a2bdaf415e9f97bba40d4
K1539 <K1539@ICT-421-WS-37>
2017-01-31 11:20:07 +0200

    t3

---
+++ Tehtava3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Tehtävä 3 - luvut tiedostoihinhome Kotitehtävä
+Tee ohjelma, joka kysyy käyttäjältä lukuja (joko kokonaisluku tai reaaliluku) ja tallenna kokonaisluvut eri tiedostoon kuin reaaliluvut. 
+Sovellus tulee lopettaa, jos käyttäjä ei syötä kokonais- tai reaalilukua. Tarkista tiedostojen sisältö jollain tekstieditorilla.
+
+Esimerkkitulostus:
+
+
+    Give a number (enter or not a number ends) : 1,0
+    Give a number (enter or not a number ends) : 2,0
+    Give a number (enter or not a number ends) : 3,0
+    Give a number (enter or not a number ends) : 4
+    Give a number (enter or not a number ends) : 5
+    Give a number (enter or not a number ends) : 6
+    Give a number (enter or not a number ends) :
+    
+    Contents of T2Integers.txt:
+    4
+    5
+    6
+
+    Contents of T2Doubles.txt:
+    1,0
+    2,0
+    3,0
+    *
+    * Jere Liikka 31.1.2017
+    */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
